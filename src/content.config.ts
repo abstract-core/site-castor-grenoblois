16c582067528a2ed8bb3537493d0711b24aa6c97
Rimarok <romaric.ruga@mailo.fr>
2026-05-31 10:24:45 +0200

    v1.0.4 : alternative page's <title>

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const pages = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/pages" }),
   schema: z.object({
     Slug: z.nullable(z.string()),
+    Title: z.nullable(z.string()),
     Description: z.nullable(z.string()),
     "Mots-clés": z.nullable(z.string()),
     Name: z.string(),
